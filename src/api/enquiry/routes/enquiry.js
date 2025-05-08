eb85f728f380e281c91b850b65fee80e058194d8
zblkartik <kartik.shikhare@zeblok.com>
2025-05-09 01:18:56 +0530

    Fixed the auth issue

--- src/api/enquiry/routes/enquiry.js
+++ src/api/enquiry/routes/enquiry.js
@@ -6,4 +6,10 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::enquiry.enquiry');
+module.exports = createCoreRouter('api::enquiry.enquiry', {
+    config: {
+        create: {
+            auth: false
+        }
+    }
+});
